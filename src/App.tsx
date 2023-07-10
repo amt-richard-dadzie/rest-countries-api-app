71a6b4d22288712d54caa5360dc4286665db4ce7
Richard Dadzie <r.dadzie@amalitech.org>
2023-07-10 18:53:59 +0000

    Create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import { MainRoutes } from "./routes/MainRoutes";
+import { Header } from "./components/Header/Header";
 
 const App = () => {
   return (
     <>
+      <Header />
       <MainRoutes />
     </>
   );
---
+++ src/components/Header/styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const Header = styled.header`
+  height: 90px;
+  box-shadow: 2px 2px 1em rgba(0, 0, 0, 0.2);
+
+  .container {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    width: 100%;
+  }
+
+  p {
+    cursor: pointer;
+  }
+`;
